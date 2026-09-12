967c80bc28360b45e5b1ef762ff6e81990281654
Weaver-K <malcolmoyieke47@gmail.com>
2026-09-12 23:37:38 +0300

    fix attemp

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
         <main className="flex-grow container mx-auto px-4 py-6">
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
-            <Route path="/Tasks" element={<Tasks />} />
-            <Route path="/About" element={<About />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
       </motion.div>
