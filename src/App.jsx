d2a60cec6027e0d57ced2395545a0b96a066d917
Weaver-K <malcolmoyieke47@gmail.com>
2026-09-12 23:26:41 +0300

    Dashboad fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Navbar />
         <main className="flex-grow container mx-auto px-4 py-6">
           <Routes>
-            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/" element={<Dashboard />} />
             <Route path="/Tasks" element={<Tasks />} />
             <Route path="/About" element={<About />} />
           </Routes>
